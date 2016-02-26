60548fff4b5fd309c7c744d1d9eaa3038fbd802c
amassanet <@codecontext>
2016-02-26 13:38:01 +0100

    Added FNMT authentication

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
     "github.com/gin-gonic/gin"
     "net/http"
     "log"
+    "crypto/x509"
+    "crypto/tls"
+    "io/ioutil"
+    "fmt"
 )
 
 func assert(err error) {
@@ -20,9 +24,37 @@ func main() {
 		})
 
 	})
+    caCertPool := x509.NewCertPool()
+    fileInfos, err := ioutil.ReadDir("certs/ca")
+    assert(err)
+    for _, fileInfo := range fileInfos {
+        if !fileInfo.IsDir() {
+            caCert, err := ioutil.ReadFile("certs/ca/"+fileInfo.Name())
+            assert(err)
+            if (!caCertPool.AppendCertsFromPEM(caCert)){
+                assert(fmt.Errorf("Failed to read ca %v", fileInfo.Name))
+            }
+        }
+    }
+
+    tlsConfig := &tls.Config{
+        // Reject any TLS certificate that cannot be validated
+        ClientAuth: tls.RequireAndVerifyClientCert,
+        // Ensure that we only use our "CA" to validate certificates
+        ClientCAs: caCertPool,
+        // PFS because we can but this will reject client with RSA certificates
+//        CipherSuites: []uint16{tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384},
+        // Force it server side
+        PreferServerCipherSuites: true,
+        // TLS 1.2 because we can
+        MinVersion: tls.VersionTLS12,
+    }
+
+
     server := &http.Server{
         Handler:        router,
+        TLSConfig:       tlsConfig,
         Addr:           ":8443",
     }
-    assert(server.ListenAndServeTLS("localhost.pem","localhost.key"))
+    assert(server.ListenAndServeTLS("certs/localhost.pem","certs/localhost.key"))
 }
