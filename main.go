75d059fb9af1294fc6f2515147e6011f5b0e02a5
amassanet <@codecontext>
2016-02-26 17:17:36 +0100

    Added websocket chat

--- main.go
+++ main.go
@@ -1,66 +1,74 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
-    "log"
-    "crypto/x509"
-    "crypto/tls"
-    "io/ioutil"
-    "fmt"
-    "strings"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 )
 
 func assert(err error) {
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-        cn := c.Request.TLS.PeerCertificates[0].Subject.CommonName
-        split := strings.Split(cn, " - NIF ")
-        name := split[0]
-        nif := split[1]
-        c.JSON(200, gin.H{
-			"name": name,
-            "nif" : nif,
-		})
+	go h.run()
 
-	})
-    caCertPool := x509.NewCertPool()
-    fileInfos, err := ioutil.ReadDir("certs/ca")
-    assert(err)
-    for _, fileInfo := range fileInfos {
-        if !fileInfo.IsDir() {
-            caCert, err := ioutil.ReadFile("certs/ca/"+fileInfo.Name())
-            assert(err)
-            if (!caCertPool.AppendCertsFromPEM(caCert)){
-                assert(fmt.Errorf("Failed to read ca %v", fileInfo.Name))
-            }
+    router := gin.Default()
+    router.LoadHTMLGlob("templates/*")
+
+    router.GET("/", func(c *gin.Context) {
+        var (
+            name string
+            nif string
+        )
+        if len(c.Request.TLS.PeerCertificates)>0 {
+            cn := c.Request.TLS.PeerCertificates[0].Subject.CommonName
+		    split := strings.Split(cn, " - NIF ")
+		    name = split[0]
+		    nif = split[1]
         }
-    }
+		c.HTML(200, "home.template",gin.H{
+		    "whoami":name +" "+nif,
+        })
+	})
+    router.GET("/ws", func(c *gin.Context) {
+        serveWs(c.Writer, c.Request)
+    })
 
-    tlsConfig := &tls.Config{
-        // Reject any TLS certificate that cannot be validated
-        ClientAuth: tls.RequireAndVerifyClientCert,
-        // Ensure that we only use our "CA" to validate certificates
-        ClientCAs: caCertPool,
-        // PFS because we can but this will reject client with RSA certificates
-//        CipherSuites: []uint16{tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384},
-        // Force it server side
-        PreferServerCipherSuites: true,
-        // TLS 1.2 because we can
-        MinVersion: tls.VersionTLS12,
-    }
+    caCertPool := x509.NewCertPool()
+	fileInfos, err := ioutil.ReadDir("certs/ca")
+	assert(err)
+	for _, fileInfo := range fileInfos {
+		if !fileInfo.IsDir() {
+			caCert, err := ioutil.ReadFile("certs/ca/" + fileInfo.Name())
+			assert(err)
+			if !caCertPool.AppendCertsFromPEM(caCert) {
+				assert(fmt.Errorf("Failed to read ca %v", fileInfo.Name))
+			}
+		}
+	}
 
+	tlsConfig := &tls.Config{
+		ClientAuth:               tls.RequestClientCert,
+		ClientCAs:                caCertPool,
+		PreferServerCipherSuites: true,
+		MinVersion:               tls.VersionTLS12,
+	}
 
-    server := &http.Server{
-        Handler:        router,
-        TLSConfig:       tlsConfig,
-        Addr:           ":8443",
-    }
-    assert(server.ListenAndServeTLS("certs/localhost.pem","certs/localhost.key"))
+	server := &http.Server{
+		Handler:   router,
+		TLSConfig: tlsConfig,
+		Addr:      ":8443",
+	}
+	assert(server.ListenAndServeTLS("certs/localhost.pem", "certs/localhost.key"))
 }
+
+
+
